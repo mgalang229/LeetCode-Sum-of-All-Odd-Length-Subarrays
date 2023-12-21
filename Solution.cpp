3156c67cebe0460688d707ae91f99a3e225a7b6d
jasonpogi <51401355+mgalang229@users.noreply.github.com>
2023-12-21 19:30:17 +0800

    Create Solution.cpp

---
+++ Solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int sumOddLengthSubarrays(vector<int>& arr) {        
+        int sum = 0, n = arr.size();
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                if ((j - i + 1) % 2 == 1) {
+                    for (int k = i; k <= j; k++) {
+                        sum += arr[k];
+                    }
+                }
+            }
+        }
+        return sum;
+    }
+};
